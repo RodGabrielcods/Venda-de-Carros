40932144fa6536ee7dc74a6cd36513920aa1bb3c
RodGabrielcods <gabriel.daniel@aluno.ifsp.edu.br>
2024-12-02 12:31:53 -0300

    Atualização 2

---
+++ src/controller/TelaRemoverFuncionarioController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class TelaRemoverFuncionarioController {
+
+    @FXML
+    private Button btnremoverfun;
+
+    @FXML
+    private Button btnsairremoverfun;
+
+    @FXML
+    private TextField tfdpffunremover;
+
+    @FXML
+    private TextField tfidfunremover;
+
+    @FXML
+    void btnremoverfunclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnsairremoverfunclick(ActionEvent event) throws IOException  {
+        URL url = getClass().getResource("/view/MenuAdmin.fxml");
+        Parent root = FXMLLoader.load(url);
+
+        Stage stgVendas = new Stage();
+        stgVendas.setTitle("Menu");
+        stgVendas.setScene(new Scene(root));
+        stgVendas.show();
+
+        Stage telaAtual = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        telaAtual.close();
+    }
+
+}
